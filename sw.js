3f0c66f9447e06334a120773f9fd3a00f8577072
td-shi <tisiphone.ready@gmail.com>
2021-12-26 18:09:33 +0900

    init.

---
+++ sw.js
@@ -0,0 +1,28 @@
+var CACHE_NAME  = "UTX120-TD";
+var urlsToCache = [
+    "https:///index.html",
+    
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(
+            function(cache){
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(
+        function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
